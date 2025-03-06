49131654ba95f912b7d96518c225758f00afedad
makanda596 <149590359+makanda596@users.noreply.github.com>
2025-03-06 17:07:15 +0300

    n

--- .history/server/routes/adminRoutes_20250306170401.js
+++ .history/server/routes/adminRoutes_20250306170401.js
@@ -2,7 +2,7 @@ import express from 'express'
 import { adminsignup, adminlogin } from '../controllers/adminControllers.js'
 const router = express.Router()
 
-router.post('/adminlogin', adminlogin)
+router.post('a/dminlogin', adminlogin)
 router.post('/adminsignup',adminsignup)
 
 export default router;
